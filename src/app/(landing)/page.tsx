9bda2b6b01ecb580cd2d919091e165df98bf1780
pimentellima <matheus_dere@hotmail.com>
2024-09-15 15:17:30 -0300

    alterado h2 em landing

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -5,25 +5,21 @@ import Link from 'next/link'
 export default function Home() {
     return (
         <main className="flex-1 bg-accent-foreground">
-            <div className="px-64 py-32 text-left text-background">
+            <div className="px-64 py-20 text-left text-background">
                 <h1 className="font-serif text-6xl">
                     Translate your resume to any language
                 </h1>
                 <h2 className="w-4/5 leading-relaxed mt-7">
-                    Ensure your resume is understood worldwide. <br />
-                    Get accurate translations tailored to your skills and
-                    experience, opening doors to global opportunities.
+                    Globalize Your Resume with Precision. <br />
+                    Ensure it's understood worldwide with precise AI-driven
+                    translations.
                 </h2>
                 <div className="flex gap-2 mt-7">
                     <Button asChild size={'lg'}>
                         <Link href="/sign-in">Get started for free</Link>
                     </Button>
-                    <Button size={'lg'} variant={'ghost'}>
-                        Check the demo
-                        <ArrowRight className="w-5 h-5 ml-2" />
-                    </Button>
                 </div>
-                <div className="h-[700px] w-full rounded-md bg-background mt-24"></div>
+                <div className="h-[700px] w-full rounded-md bg-background mt-20"></div>
             </div>
         </main>
     )
