94b40e1cb1282f43e5058e8414fdb3cc8bcbf738
pimentellima <matheus_dere@hotmail.com>
2024-09-24 20:03:08 -0300

    alterada fonte serif

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,9 @@ module.exports = {
             },
         },
         extend: {
+            fontFamily: {
+                serif: ['Georgia', 'sans serif'],
+            },
             colors: {
                 border: 'hsl(var(--border))',
                 input: 'hsl(var(--input))',
