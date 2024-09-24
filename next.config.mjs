67a3bc53a0d2ea85fac030375b6eac9c1604d94c
pimentellima <matheus_dere@hotmail.com>
2024-09-24 20:33:57 -0300

    noDeprecation

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,13 @@ const nextConfig = {
     experimental: {
         serverComponentsExternalPackages: ['@aws-sdk'],
     },
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            // Suppress deprecation warnings globally
+            process.noDeprecation = true
+        }
+        return config
+    },
 }
 
 export default nextConfig
