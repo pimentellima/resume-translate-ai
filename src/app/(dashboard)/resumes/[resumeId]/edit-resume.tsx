b11c03ab32b75c4b487005ee691d19d8db250f81
pimentellima <matheus_dere@hotmail.com>
2024-09-30 23:08:36 -0300

    fix: corrigido layout da página do curriculo no mobile

--- src/app/(dashboard)/resumes/[resumeId]/edit-resume.tsx
+++ src/app/(dashboard)/resumes/[resumeId]/edit-resume.tsx
@@ -7,7 +7,6 @@ import { useState } from 'react'
 import ButtonDownloadFile from './button-download-file'
 import PdfVisualizer from './pdf-visualizer'
 import SelectResumeLanguage from './select-resume-language'
-import { useSession } from 'next-auth/react'
 import ButtonDeleteResume from '../button-delete-resume'
 
 export default function EditResume({
@@ -20,32 +19,27 @@ export default function EditResume({
     pdfUrl: string
 }) {
     const [loading, setLoading] = useState(false)
-    const session = useSession()
 
     return (
-        <div className="w-[700px]">
-            <div className="flex flex-col justify-between gap-1 px-16 md:flex-row md:px-0">
-                {session?.data?.user && (
-                    <Button size={'lg'} asChild variant={'outline'}>
-                        <Link href={'/resumes'}>
-                            <ArrowLeft className="w-5 h-5 mr-2" /> Resumes
-                        </Link>
-                    </Button>
-                )}
-                <div className='flex gap-2'>
-                    <SelectResumeLanguage
-                        setLoading={setLoading}
-                        resumeId={resumeId}
-                        selectedLanguage={language}
-                    />
-                    <ButtonDownloadFile
-                        fileUrl={pdfUrl}
-                        disabled={!language}
-                        label="Download"
-                        size={'lg'}
-                    />
-                    <ButtonDeleteResume resumeId={resumeId} size={'lg'} />
-                </div>
+        <div className="w-full md:w-[700px]">
+            <div className="px-4 flex flex-col md:flex-row gap-1">
+                <Button size={'lg'} asChild variant={'outline'}>
+                    <Link href={'/resumes'}>
+                        <ArrowLeft className="w-5 h-5 mr-2" /> Resumes
+                    </Link>
+                </Button>
+                <SelectResumeLanguage
+                    setLoading={setLoading}
+                    resumeId={resumeId}
+                    selectedLanguage={language}
+                />
+                <ButtonDownloadFile
+                    fileUrl={pdfUrl}
+                    disabled={!language}
+                    label="Download"
+                    size={'lg'}
+                />
+                <ButtonDeleteResume resumeId={resumeId} size={'lg'} />
             </div>
             <PdfVisualizer
                 translated={!!language}
